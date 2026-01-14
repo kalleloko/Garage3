6a5cffcd292d31844d215493564115d0603b1235
Kalle Lindqvist <kallelin@gmail.com>
2026-01-14 16:40:13 +0100

    Add project files.

---
+++ Garage3/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Garage3.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
