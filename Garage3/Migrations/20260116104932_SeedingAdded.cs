7918bdd6fc00af807a761298ef9c9b7d32ada2ae
Erik <erre.siljestrom@gmail.com>
2026-01-16 11:50:20 +0100

    Added migration for seeding

---
+++ Garage3/Migrations/20260116104932_SeedingAdded.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage3.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SSN",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_SSN",
+                table: "AspNetUsers",
+                column: "SSN",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_SSN",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SSN",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
