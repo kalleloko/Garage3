f049ee44e5b36951e370819bfb62fc8e116693da
Kjell <kjell314@hotmail.com>
2026-01-16 15:53:16 +0100

    Migration

---
+++ Garage3/Migrations/20260116145141_Misc.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage3.Migrations
+{
+    /// <inheritdoc />
+    public partial class Misc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SpotSize",
+                table: "ParkingSpots");
+
+            migrationBuilder.RenameColumn(
+                name: "SpotNUmber",
+                table: "ParkingSpots",
+                newName: "SpotNumber");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SpotNumber",
+                table: "ParkingSpots",
+                newName: "SpotNUmber");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SpotSize",
+                table: "ParkingSpots",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
